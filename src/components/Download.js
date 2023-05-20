b5fa6b736f6d2afbdb3cd7c72023e28c063f328e
Libra <73041325+Librv@users.noreply.github.com>
2023-05-20 14:48:19 +0200

    Move Download style to css, increase font size

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,30 +3,12 @@ import React from 'react';
 // Have download counter
 
 function card(site, link) {
-    return (<a href={link}><div style={{
-        display: 'inline-block',
-        verticalAlign: 'middle',
-        marginInline: '.75rem',
-        width: '18rem'
-    }} className="card card-button">
+    return (<a href={link}><div className="card card-button">
         <div className="card__header">
             <span className={'download-' + site} />
         </div>
     </div></a>);
-
-    /** <div class="card__body">
-            <Markdown components={{ p: React.Fragment }} children={markdown}/>
-        </div> */
-    /**<div class="card__footer">
-            <button class="button button--secondary button--block">See All</button>
-        </div> */
 }
-
-/*export default function Download({ typeCurseForge, typeModrinth, project }) {
-    return (<div style={{ textAlign: 'center', margin: '2rem' }}>
-        <Download type={typeCurseForge} project={project}></Download><Modrinth type={typeModrinth} project={project}></Modrinth>
-    </div>);
-}*/
 
 export function Curseforge({ type, project }) {
     var link = "https://www.curseforge.com/minecraft/" + type + "/" + project;
